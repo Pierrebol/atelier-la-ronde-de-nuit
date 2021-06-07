3768ad613b773489b27b1d28718b08bdb9fa3ffb
pierrebollack <pierre.bollack@edhec.com>
2021-06-07 23:14:10 +0200

    accueil page html/css

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#accueil', as: :accueil
+  get 'restauratrice', to: 'pages#restauratrice', as: :restauratrice
+  get 'prestations', to: 'pages#prestations', as: :prestations
+  get 'galerie', to: 'pages#galerie', as: :galerie
+  get 'devis', to: 'pages#devis', as: :devis
+  get 'contact', to: 'pages#contact', as: :contact
 end
